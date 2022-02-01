5303c7ccbacb137f7cbb5c43644ec61235862818
Boaz <42014936+boazmenard@users.noreply.github.com>
2022-02-01 18:16:11 -0500

    store message components

---
+++ mixed-messages-app.js
@@ -0,0 +1,7 @@
+let animeCharacters = ['Eren Jaeger', 'Naruto Uzamaki', 'Tanjiro Kamado', 'Yuji Itadori', 'Kirito', 'Goku', 'Mikasa Ackerman']
+let adjectives = ['cool', 'awesome', 'weird', 'beautiful', 'strong', 'swole', 'unbeatable', 'crazy', 'smart']
+let emojis = [';)', ':)', '-_-', '0_0', ':^(', ':(', ':/']
+
+const characterSelector = Math.floor(Math.random() * animeCharacters.length)
+const adjectiveSelector = Math.floor(Math.random() * adjectives.length)
+const emojiSelector = Math.floor(Math.random() * emojis.length)
