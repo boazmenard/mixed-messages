7f66dad5ab10bb8774e366ee39ad1b3abe4e0b8a
Boaz <42014936+boazmenard@users.noreply.github.com>
2022-02-01 18:30:32 -0500

    create message and put it all together

--- mixed-messages-app.js
+++ mixed-messages-app.js
@@ -5,3 +5,10 @@ let emojis = [';)', ':)', '-_-', '0_0', ':^(', ':(', ':/']
 const characterSelector = Math.floor(Math.random() * animeCharacters.length)
 const adjectiveSelector = Math.floor(Math.random() * adjectives.length)
 const emojiSelector = Math.floor(Math.random() * emojis.length)
+
+function giveMeInspirationalAnimeMessage() {
+    return `HEY YOU! I want you to know that you are ${adjectives[adjectiveSelector].toUpperCase()} like ${animeCharacters[characterSelector].toUpperCase()}!!! ${emojis[emojiSelector].toUpperCase()}`
+}
+
+console.log('I know you need this right now...')
+console.log(giveMeInspirationalAnimeMessage())
